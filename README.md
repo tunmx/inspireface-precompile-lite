d7e55bc4fb6a8efa7edfba44baf9adae9c7ad05a
Jingyu <tunmxy@163.com>
2024-11-18 00:00:51 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+
+## Libs
+- **RKNN1**: [https://github.com/airockchip/rknpu](https://github.com/airockchip/rknpu)
+- **RKNN2**: [https://github.com/airockchip/rknn-toolkit2/tree/master/rknpu2](https://github.com/airockchip/rknn-toolkit2/tree/master/rknpu2)
+- **SQLite**: [https://github.com/sqlite/sqlite](https://github.com/sqlite/sqlite)
+- **SQLite-Vec**: [https://github.com/asg017/sqlite-vec](https://github.com/asg017/sqlite-vec)
+
---
+++ sqlite/sqlite-vec.h
@@ -0,0 +1,35 @@
+#ifndef SQLITE_VEC_H
+#define SQLITE_VEC_H
+
+#ifndef SQLITE_CORE
+#include "sqlite3ext.h"
+#else
+#include "sqlite3.h"
+#endif
+
+#ifdef SQLITE_VEC_STATIC
+  #define SQLITE_VEC_API
+#else
+  #ifdef _WIN32
+    #define SQLITE_VEC_API __declspec(dllexport)
+  #else
+    #define SQLITE_VEC_API
+  #endif
+#endif
+
+#define SQLITE_VEC_VERSION "v0.1.3"
+#define SQLITE_VEC_DATE "2024-09-26T07:27:42Z+0000"
+#define SQLITE_VEC_SOURCE "496560cf9ac4b358ea43793e591f376c02c16b90"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+SQLITE_VEC_API int sqlite3_vec_init(sqlite3 *db, char **pzErrMsg,
+                  const sqlite3_api_routines *pApi);
+
+#ifdef __cplusplus
+}  /* end of the 'extern "C"' block */
+#endif
+
+#endif /* ifndef SQLITE_VEC_H */
